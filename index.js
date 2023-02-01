f37f766975e652a0271b30dea740735322ab3523
skandertebo <alexandertebourb@gmail.com>
2023-02-01 02:45:13 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,165 @@
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.getRegistration().then(function(registration) {
+        if (!registration) {
+            navigator.serviceWorker.register('sw.js', {scope: './'}).then(function(registration) {
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, function(err) {
+                console.log('ServiceWorker registration failed: ', err);
+            });
+        }
+    });
+}
+
+let n = 0 , v = 0;
+let pendingVScore, pendingNScore;
+
+renderScore(n,v);
+
+
+
+document.querySelectorAll('input[type=checkbox]').forEach(function(e){
+    e.addEventListener('change', function(event){
+        let team = e.id[0];
+        if(e.id.startsWith("belote")){
+            if(e.checked){
+                let letter = e.id.split('-')[1] === 'n'? 'v' : 'n';
+                let otherbelote = document.getElementById('belote-'+letter);
+                if(otherbelote.checked){
+                    otherbelote.checked = false;
+                }
+            }
+        }
+        if(e.checked){
+            if(e.id[0] == 'n'){
+                //uncheck all n
+                document.querySelectorAll('input[id^=n]').forEach(function(e){
+                    e.checked = false;
+                });
+                //disable number input for n
+                document.getElementById('score-n').disabled = true;
+            }
+            else if(e.id[0] == 'v'){
+                //uncheck all v
+                document.querySelectorAll('input[id^=v]').forEach(function(e){
+                    e.checked = false;
+                });
+                //disable number input for v
+                document.getElementById('score-v').disabled = true;
+            }
+            e.checked = true;
+            
+            let name = e.id.split('-')[1];
+            let pendingScore;
+            switch(name){
+                case 'contre':
+                    pendingScore = 320;
+                    break;
+                case 'surcontre':
+                    pendingScore = 640;
+                    break;
+                case 'capot':
+                    pendingScore = 500;
+                    break;
+                case 'dedans':
+                    pendingScore = 160;
+                    break;
+            }
+            if(team == 'n'){
+                pendingNScore = pendingScore;
+            }
+            else if(team == 'v'){
+                pendingVScore = pendingScore;
+            }
+        }
+        else{
+            if(e.id[0] == 'n'){
+                //enable number input for n
+                document.getElementById('score-n').disabled = false;
+                pendingNScore = document.getElementById('score-n').value;
+            }
+            else if(e.id[0] == 'v'){
+                //enable number input for v
+                document.getElementById('score-v').disabled = false;
+                pendingVScore = document.getElementById('score-v').value;
+            }
+        }
+    });
+});
+
+document.getElementById('score-n').addEventListener('input', function(event){
+    pendingNScore = event.target.value;
+});
+
+document.getElementById('score-v').addEventListener('input', function(event){
+    pendingVScore = event.target.value;
+});
+
+document.getElementById('add-score-v').addEventListener('click', function(event){
+    if(pendingVScore){
+        v += parseInt(pendingVScore);
+        if(!document.getElementById('score-v').disabled){
+            if(document.getElementById('v-case').value==="case"){
+                n += 170 - pendingVScore;
+            }
+            else{
+                n += 160 - pendingVScore;
+            }
+        }
+        if(document.getElementById('belote-n').checked){
+            if(pendingVScore > 250){
+                v += 20;
+            }else{
+                n += 20;
+            }
+        }
+        if(document.getElementById('belote-v').checked){
+            v += 20;
+        }
+        renderScore(n,v);
+        //uncheck all v
+        document.querySelectorAll('input[id^=v]').forEach(function(e){
+            e.checked = false;
+        });
+        //empty number input for v
+        document.getElementById('score-v').value = '';
+        document.getElementById('score-v').disabled = false;
+    }
+});
+
+
+document.getElementById('add-score-n').addEventListener('click', function(event){
+    if(pendingNScore){
+        n += parseInt(pendingNScore);
+        if(!document.getElementById('score-n').disabled){
+            if(document.getElementById('n-case').value==="case"){
+                v += 170 - parseInt(pendingNScore);
+            }
+            else{
+                v += 160 - parseInt(pendingNScore);
+            }
+        }
+        if(document.getElementById('belote-v').checked){
+            if(pendingNScore > 250){
+                n += 20;
+            }else{
+                v += 20;
+            }
+        }
+        if(document.getElementById('belote-n').checked){
+            n += 20;
+        }
+        renderScore(n,v);
+        //uncheck all n
+        document.querySelectorAll('input[id^=n]').forEach(function(e){
+            e.checked = false;
+        });
+        //empty number input for n
+        document.getElementById('score-n').value = '';
+        document.getElementById('score-n').disabled = false;
+    }
+});
+
+function renderScore(n,v){
+    document.getElementById('display-n-score').innerHTML = n;
+    document.getElementById('display-v-score').innerHTML = v;
+}
---
+++ sw.js
@@ -0,0 +1,31 @@
+// cache files
+var cacheName = 'v1';
+var cacheFiles = [
+    './',
+    './index.html',
+    './style.css',
+    './index.js',
+    'logo.png'
+]
+// install event
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Installed');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching cacheFiles');
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+
+// cache first strategy
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+        // Cache hit - return response
+        if (response) { return response; }
+        return fetch(event.request);
+        })
+    );
+});
